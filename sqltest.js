719e05a328a1bbed46df0c7326b958c4bac9261e
psavira <savi2586@stthomas.edu>
2022-03-10 19:14:57 -0600

    test add-course and get-all-courses methods

---
+++ sqltest.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+var config =
+{
+    host: 'capstonedb01.mysql.database.azure.com',
+    user: 'webdevteam',
+    password: 'webdevpass',
+    database: 'classyschedule',
+    port: 3306,
+    //ssl: {ca: fs.readFileSync("C:/Users/btbor/Web Dev/sqltest/DigiCertGlobalRootCA.crt.pem")}
+};
+
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+    function (err) { 
+    if (err) { 
+        console.log("!!! Cannot connect !!! Error:");
+        throw err;
+    }
+    else
+    {
+       console.log("Connection established.");
+      queryDatabase();
+    }
+});
+
+function queryDatabase(){
+  conn.query('SELECT * FROM Class', function(err, results, fields) {
+  // conn.query('DESCRIBE Class', function(err, results, fields) {
+    if(err) throw err;
+    console.log(JSON.stringify(results));
+  })
+}
