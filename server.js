6f3baf6e3dd81c257acd22265d7914f71e0210e7
PokkeFe <decerez@gmail.com>
2022-04-02 21:58:32 -0500

    Added handlebars and set up routing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 // imports
 const express = require("express");
+const hbs = require('express-hbs');
 
 // environment variables setup
 require('dotenv').config()
@@ -11,6 +12,13 @@ const apiRouter = require("./routes/api")
 
 // express app setup
 const app = express();
+
+// setup express views [handlebars]
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 // express middleware
 app.use(express.json())
@@ -23,9 +31,33 @@ const PORT = process.env.PORT || 3000;
 // routes
 app.use('/api', apiRouter)
 
-app.get("/",(req, res) => {
-    res.sendFile(index.html);
+app.get("/", (req, res) => {
+    res.render('index')
 });
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/help', (req, res) => {
+    res.render('help')
+})
+
+app.get('/schedule', (req, res) => {
+    res.render('schedule')
+})
+
+app.get('/classEntry', (req, res) => {
+    res.render('classEntry');
+})
+
+app.get('/professorEntry', (req, res) => {
+    res.render('professorEntry');
+})
+
+app.get('/roomEntry', (req, res) => {
+    res.render('roomEntry');
+})
 
 app.listen(PORT,() => {
     console.log('App up at port %s', PORT);
