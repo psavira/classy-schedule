0eb4ea6529ea3237b011af0508284defa77ebda8
psavira <savi2586@stthomas.edu>
2022-04-13 19:31:57 -0500

    \linked login to signup

--- routes/main.js
+++ routes/main.js
@@ -72,8 +72,4 @@ router.get('/addRoom', authLocals, isAuthorized, (req, res) => {
     res.render('addRoom');
 })
 
-router.get('/signup', authLocals, isAuthorized, (req, res) => {
-    res.render('signup');
-})
-
 module.exports = router
