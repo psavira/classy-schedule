eed9f92271cff73a585a6bf834a9a84c542daa8f
Cole Potter <decerez@gmail.com>
2022-05-09 16:12:30 -0500

    Removed test python code

--- server.js
+++ server.js
@@ -53,41 +53,5 @@ app.listen(PORT, () => {
     console.log('App up at port %s', PORT);
 });
 
-/* ------------------------------- Test Python ------------------------------ */
-
-// Make some fake data
-let fake_data = {
-    'name': 'bob',
-    'age': '25'
-}
-
-// write the fake data to a file
-fs.writeFileSync('./scheduler-data/testin.json', JSON.stringify(fake_data), {flag: 'w+'})
-
-// Spawn a new process running the python script
-console.log('Spawning python process...');
-let python_script = spawn('python', ['./python/test_rw.py'])
-
-// Print python output from node
-python_script.stdout.on('data', (chunk) => {
-    console.log(`python stdin: ${chunk}`);
-})
-
-python_script.stderr.on('data', (chunk) => {
-    console.error(`python stderr: ${chunk}`);
-})
-
-// When the python script exists, log the exit code
-python_script.on('exit', (code, signal) => {
-    console.log(`Python script exited with code ${code}`);
-    // output fake data if error code good
-    if (code == 0) {
-        let testout = JSON.parse(fs.readFileSync('./scheduler-data/testout.json'));
-        console.log(testout);
-    } else {
-        console.error('Python script didn\'t exit properly');
-    }
-
-})
 
 module.exports = app;
