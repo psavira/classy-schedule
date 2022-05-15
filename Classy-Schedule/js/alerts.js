e6d6723940f29784c79a9ebe5bf501a626a03b15
PokkeFe <decerez@gmail.com>
2022-05-15 00:38:00 -0500

    Update alerts

--- Classy-Schedule/js/alerts.js
+++ Classy-Schedule/js/alerts.js
@@ -2,29 +2,30 @@
  * Displays an alert box to the user
  * @param alertText Text that appears in the alert box
  */
- function showAlert(alertText) {
-    // get alert container to hold alerts
-    const alertContainer = document.getElementById('alertContainer');
-    // make alert container to hold alerts
-    const alert = document.createElement('div');
-    // add callouts and warnings to alert
-    alert.classList.add('callout', 'warning');
-    alert.innerText = alertText;
-    // add alert to container
-    alertContainer.appendChild(alert);
-  }
-  
-  /** Clears all current alerts */
-  function clearAlerts() {
-    // get alert container to hold alerts
-    const alertContainer = document.getElementById('alertContainer');
-    // assing children of alert
-    const children = [...alertContainer.children];
-    // loop through alerts
-    children.forEach((child) => {
-      // print out alerts
-      console.log(child);
-      // remove alert that got printed
-      alertContainer.removeChild(child);
-    });
-  }
+function showAlert(alertText, alertClass) {
+  // if defined, use parameter alert class. Otherwise, use 'alert'
+  alertClass = alertClass || 'alert';
+  // container to hold alerts
+  const alertContainer = document.getElementById('alertContainer');
+  // make alert a div element
+  const alert = document.createElement('div');
+  // add callout and alert class to alert
+  alert.classList.add('callout', alertClass);
+  alert.innerText = alertText;
+  // add alert to alert container
+  alertContainer.appendChild(alert);
+}
+
+/** Clears all current alerts */
+function clearAlerts() {
+  // make a container to hold alerts
+  const alertContainer = document.getElementById('alertContainer');
+  // array of alerts
+  const children = [...alertContainer.children];
+  // eslint-disable-next-line no-restricted-syntax
+  // loop through the alerts
+  children.forEach((child) => {
+    // remove the alerts
+    alertContainer.removeChild(child);
+  });
+}
